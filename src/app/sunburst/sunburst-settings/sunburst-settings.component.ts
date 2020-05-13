dbe1b4e670de1f5f17f699440aa83a1520a1474c
Marcel Hrnecek <recek.se@gmail.com>
2020-05-13 17:28:38 +0200

    refactor general and text settings and integrate data settings

--- src/app/sunburst/sunburst-settings/sunburst-settings.component.ts
+++ src/app/sunburst/sunburst-settings/sunburst-settings.component.ts
@@ -8,10 +8,7 @@ import {ChartDataService} from "../../services/chart-data.service";
 })
 export class SunburstSettingsComponent implements OnInit {
 
-  data: any;
-  yes_color: string;
-  no_color: string;
-  real_ano_value: number;
+  data: MainData;
 
   constructor(
     private chartDataService: ChartDataService
@@ -22,9 +19,6 @@ export class SunburstSettingsComponent implements OnInit {
     this.chartDataService.sunburstData$
       .subscribe(data => {
         this.data = data;
-        this.yes_color = this.data.children[0].color;
-        this.no_color = this.data.children[1].color;
-        this.real_ano_value = this.data.children[0].children[1].value;
 
       });
   }
@@ -33,29 +27,4 @@ export class SunburstSettingsComponent implements OnInit {
     this.chartDataService.updateSunburstData(this.data);
   }
 
-  updateYesColor(newColor: string) {
-    this.updateColorData(this.data, newColor, 'yes');
-    this.updateData();
-  }
-
-  updateNoColor(newColor: string) {
-    this.updateColorData(this.data, newColor, 'no');
-    this.updateData();
-  }
-
-  private updateColorData(data, newColor: string, type: string) {
-    data.children.forEach(dataPoint => {
-      if (dataPoint.color_type === type) {
-        dataPoint.color = newColor;
-      }
-      if (dataPoint.children) {
-        this.updateColorData(dataPoint, newColor, type);
-      }
-    });
-  }
-
-  updateRealAnoValue(anoValue: number) {
-    this.data.children[0].children[1].value = anoValue;
-    this.updateData();
-  }
 }
